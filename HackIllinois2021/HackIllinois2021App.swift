ff78498c65976df6f32f1d46940583f5448536eb
Kevin Yan <guanwenyan301@gmail.com>
2021-07-21 20:48:14 +0800

    Initial Commit

---
+++ HackIllinois2021/HackIllinois2021App.swift
@@ -0,0 +1,17 @@
+//
+//  HackIllinois2021App.swift
+//  HackIllinois2021
+//
+//  Created by Guanwen yan on 7/21/21.
+//
+
+import SwiftUI
+
+@main
+struct HackIllinois2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
